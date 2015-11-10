2f57c56ae8c2b0b58a1b64d8cc010ffa425475d9
Guillaume Vincent <guillaume@oslab.fr>
2015-11-10 19:36:04 +0100

    improve help method for Autocomplete class

--- app/autocomplete.js
+++ app/autocomplete.js
@@ -1,21 +1,27 @@
 class AutoComplete {
-    constructor(workers) {
-        this.workers = workers;
+    constructor(minions = []) {
+        this._minions = minions;
     }
 
-    help() {
+    help(question = '') {
         var answers = [];
-        if (this.workers) {
-            this.workers.forEach(function (worker) {
-                answers.push(worker.mine())
+        this._get_minions(question).forEach(function (minion) {
+            minion.mine().forEach(function (answer) {
+                answers.push(answer)
             });
-        }
+        });
         return answers;
+    }
+
+    _get_minions(filter = '') {
+        return this._minions.filter(function (minion) {
+            return filter.indexOf(minion.trigger) > -1
+        })
     }
 }
 
 class Minion {
-    constructor(trigger) {
+    constructor(trigger = '') {
         this.trigger = trigger;
     }
 
--- tests/test_autocomplete.js
+++ tests/test_autocomplete.js
@@ -6,9 +6,9 @@ describe('autocomplete', function () {
         var answers = autocomplete.help();
         assert(Array.isArray(answers));
     });
-    it('should have some workers', function () {
+    it('should have some _minions', function () {
         var autocomplete = new AutoComplete([{}, {}]);
-        assert.equal(2, autocomplete.workers.length);
+        assert.equal(2, autocomplete._minions.length);
     });
     it('should call mine from minion', function (done) {
         var minion = new Minion();
@@ -17,6 +17,32 @@ describe('autocomplete', function () {
         };
         var autocomplete = new AutoComplete([minion]);
         autocomplete.help();
+    });
+    it('should return answers depending on the trigger', function () {
+        var minion1 = new Minion('&');
+        minion1.mine = function () {
+            return ['a1']
+        };
+        var minion2 = new Minion('#');
+        minion2.mine = function () {
+            return ['a2']
+        };
+        var autocomplete = new AutoComplete([minion1, minion2]);
+        var anwsers = autocomplete.help('#');
+        assert.deepEqual(['a2'],anwsers)
+    });
+    it('should return availables minions', function () {
+        var minion1 = new Minion('&');
+        var minion2 = new Minion('#');
+        var autocomplete = new AutoComplete([minion1, minion2]);
+        var minions = autocomplete._get_minions('#');
+        assert.deepEqual([minion2],minions)
+    });
+    it('should return one minion even if trigger doesnt match', function () {
+        var minion1 = new Minion();
+        var autocomplete = new AutoComplete([minion1]);
+        var minions = autocomplete._get_minions();
+        assert.deepEqual([minion1],minions)
     });
 });
 
